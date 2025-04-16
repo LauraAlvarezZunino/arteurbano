ea2251e1ecc9b2bebc86c62e20efe442899de714
LauraAlvarezZunino <lauraalvarezzunino@gmail.com>
2025-04-16 15:43:09 -0300

    borro codigo sin uso de css, agrego documento js

---
+++ script.js
@@ -0,0 +1,21 @@
+const colorInput = document.getElementById("color");
+
+colorInput.addEventListener("input", function () {
+  const selectedColor = this.value;
+  const form = document.querySelector("form");
+
+  // Convertimos el color HEX a RGBA x la opacidad
+  function hexToRGBA(hex, alpha = 0.5) {
+    let r = parseInt(hex.slice(1, 3), 16);
+    let g = parseInt(hex.slice(3, 5), 16);
+    let b = parseInt(hex.slice(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  }
+
+  const color1 = hexToRGBA(selectedColor, 0.7);
+  const color2 = hexToRGBA(selectedColor, 0.1);
+
+  // Aplicar gradiente como fondo al form
+  form.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
+
+});
