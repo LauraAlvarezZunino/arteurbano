2a1995600ab6fce10ea5dbec43ad8f4ffe0d39cc
LauraAlvarezZunino <lauraalvarezzunino@gmail.com>
2025-04-17 14:22:10 -0300

    se modifica color de background form , y codigo de js para cambio de background de form

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-//pendiente modificacion 
-const colorInput = document.getElementById("color");
-
-colorInput.addEventListener("input", function () {
-  const selectedColor = this.value;
-  const form = document.querySelector("form");
-
-  //esto lo tengo que modificar para seguir usando hexa!  
-  function hexToRGBA(hex, alpha = 0.5) {
-    let r = parseInt(hex.slice(1, 3), 16);
-    let g = parseInt(hex.slice(3, 5), 16);
-    let b = parseInt(hex.slice(5, 7), 16);
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-  }
-
-  const color1 = hexToRGBA(selectedColor, 0.7);
-  const color2 = hexToRGBA(selectedColor, 0.1);
-
-  // Aplicar gradiente como fondo al form
-  form.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
-
+document.getElementById('color').addEventListener('input', (e) => {
+  document.querySelector('form').style.backgroundColor = `${e.target.value}4d`;
 });
+//e Representa el evento(en este caso es input) y e.target es el input de color
