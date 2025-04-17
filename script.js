982eeb596407269b27b41f8f7b637d2ddcd79c81
LauraAlvarezZunino <lauraalvarezzunino@gmail.com>
2025-04-17 14:26:26 -0300

    modifico opacidad bgform

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('color').addEventListener('input', (e) => {
-  document.querySelector('form').style.backgroundColor = `${e.target.value}4d`;
+  document.querySelector('form').style.backgroundColor = `${e.target.value}80`;
 });
 //e Representa el evento(en este caso es input) y e.target es el input de color
